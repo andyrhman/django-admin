0b3acd1b7dcae2685b5e3dc2da9ff41c72ce72f3
andyrhman <andyrahman989@gmail.com>
2024-05-03 20:05:22 +0800

    Added get order total

--- orders/serializers.py
+++ orders/serializers.py
@@ -9,6 +9,11 @@ class OrderItemSerializer(serializers.ModelSerializer):
         
 class OrderSerializer(serializers.ModelSerializer):
     order_items = OrderItemSerializer(many=True)
+    total = serializers.SerializerMethodField('get_total')
+    
+    def get_total(self, obj):
+        items = OrderItem.objects.all().filter(order_id=obj.id)
+        return sum((o.price) for o in items)
     class Meta:
         model = Order
         fields = "__all__"
